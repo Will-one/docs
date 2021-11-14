08823f6e91e6dda9c112cd9e38f0b5603147d22e
Willone <w101372@163.com>
2021-11-14 20:27:01 +0700

    p3

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,37 @@
 module.exports = {
     title: 'Hello VuePress',
-    description: 'Just playing around'
+    description: 'Just playing around',
+    themeConfig: {
+        logo: '/assets/img/logo.png',
+        nav: [
+            {
+                text: 'Languages',
+                ariaLabel: 'Language Menu',
+                items: [
+                    { text: 'Chinese', link: '/language/chinese/' },
+                    { text: 'Japanese', link: '/language/japanese/' }
+                ]
+            },
+            { text: 'Home', link: '/' },
+            { text: 'About', link: '/about/' },
+            {
+                text: '了解更多',
+                items: [
+                    {
+                        text: 'Group1', items: [
+                            { text: 'Home', link: '/' },
+                            { text: 'About', link: '/about/' },
+                        ]
+                    },
+                    {
+                        text: 'Group2', items: [
+                            { text: 'Chinese', link: '/language/chinese/' },
+                            { text: 'Japanese', link: '/language/japanese/' }
+                        ]
+                    }
+                ]
+            },
+            { text: 'External', link: 'https://google.com' },
+        ]
+    }
 }
