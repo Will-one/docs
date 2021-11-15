d90766c82f358f452e4f6981649ec9c00719c2ad
wangzixuan <wangzixuan@ruilibank.com>
2021-11-15 16:10:07 +0700

    引入pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,14 +8,31 @@ module.exports = {
         ['meta', { name: 'author', content: 'Willone' }],
         ['meta', { name: 'keywords', content: 'vuepress,博客,前端' }],
         ['link', { rel: 'icon', href: '/favicon.ico' }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['link', { rel: 'apple-touch-icon', href: '/icons/icon-152x152.png' }],
+        // ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+        ['meta', { name: 'msapplication-TileImage', content: '/icons/icon-144x144.png' }],
+        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
     ],
     plugins: [
         [
-            '@vuepress/last-updated',
-            {
+            '@vuepress/last-updated', {
                 transformer: (timestamp, lang) => {
                     moment.locale(lang)
                     return moment(timestamp).format('LLLL')
+                }
+            }
+        ],
+        [
+            '@vuepress/pwa', {
+                serviceWorker: true,
+                // pwa新内容popup
+                updatePopup: {
+                    message: "新的内容可用",
+                    buttonText: "刷新"
                 }
             }
         ]
